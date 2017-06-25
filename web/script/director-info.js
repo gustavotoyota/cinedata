4f6eccd770ff73d3efc26833220a7e5205ff6425
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2017-06-25 08:31:38 -0300

    Correção dos scripts

--- web/script/director-info.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    getImage("James Gunn", true, 300, function (image) {
-        $("#director-img").css("background-image", "url('" + image + "')");
-    });
-});
--- web/script/movie-info.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    getImage("Guardiões da Galáxia Vol. 2", false, 300, function (image) {
-        $("#movie-img").css("background-image", "url('" + image + "')");
-    });
-});
